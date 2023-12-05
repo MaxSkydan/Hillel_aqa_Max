16d6ae61d61a8f2638122432d82734f4e639590d
MaxSkydan <skidan.maxim126@gmail.com>
2023-12-05 16:02:55 +0200

    first program

--- first_program.py
+++ first_program.py
@@ -1,9 +1,14 @@
+"""A long time ago in a galaxy far, far away..."""
+
 anakin = 'Hello There'
 print(anakin)
 print(id(anakin))
+
 darthvader = anakin
 print(darthvader)
 print(id(darthvader))
+
 del anakin
+
 print(darthvader)
 print(id(darthvader))
