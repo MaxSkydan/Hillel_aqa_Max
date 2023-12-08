dcf069828eaca186d100cdbe285db3d5288e5848
MaxSkydan <skidan.maxim126@gmail.com>
2023-12-08 15:28:10 +0200

    hometask

---
+++ hometask3.py
@@ -0,0 +1,34 @@
+"""Hometask format and f-strings."""
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(message)s')
+
+# we have norway text in old style formatting
+# re-write the same text as:
+# #1 string with format() call
+# #2 f-string
+# use linter(https://github.com/wemake-services/wemake-python-styleguide)
+# to check your new created python module for possible linter errors
+# try to run code from pycharm/command line
+
+norway_text = 'Automatisering akselererer %syeblikket da roboter vil erobre planeten v%sr. (%s)' % ('ø', 'å', 'Æ')
+logging.info(norway_text)
+
+"""Format"""
+text_format = 'Automatisering akselererer {0}yeblikket da roboter vil erobre planeten v{1}r. ({2})'
+logging.info(text_format.format('ø', 'å', 'Æ'))
+# another format
+new_text = 'Automatisering akselererer {0}yeblikket da roboter vil erobre planeten v{1}r. ({2})'.format('ø', 'å', 'Æ')
+logging.info(new_text)
+
+"""f-string"""
+text_f_string = f"Automatisering akselererer {'ø'}yeblikket da roboter vil erobre planeten v{'å'}r. ({'Æ'})"
+logging.info(text_f_string)
+
+# another f-string
+var1, var2, var3 = 'ø', 'å', 'Æ'
+text_f_string = f"""Automatisering akselererer {var1}yeblikket da roboter vil erobre planeten v{var2}r. ({var3})"""
+logging.info(text_f_string)
+# f-string with variables name
+text_f_string = f"""Automatisering akselererer {var1=}yeblikket da roboter vil erobre planeten v{var2=}r. ({var3=})"""
+logging.info(text_f_string)
