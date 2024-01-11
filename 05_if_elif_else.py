34c34fa1738e4cbf3e02b30217ced5c4dfa6f131
MaxSkydan <120572692+MaxSkydan@users.noreply.github.com>
2024-01-11 17:52:30 +0200

    home task (#6)
    * home task
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

---
+++ 05_if_elif_else.py
@@ -0,0 +1,86 @@
+"""
+Home task 7. If elif else.
+
+given values w,x,y,z
+write if-elif-else statement that will search minimum value and
+print smth aka 'y' is minimum value'
+where 'y' is variable name (not value)
+advice use python debugger and different values to check your algorithm
+optionally you can check your algorithm somehow with assert statements.
+"""
+
+import logging
+
+_log = logging.getLogger()
+console_handler = logging.StreamHandler()
+log_format = logging.Formatter('[%(asctime)s %(levelname)s]: %(message)s')
+console_handler.setFormatter(log_format)
+_log.addHandler(console_handler)
+_log.setLevel(logging.DEBUG)
+
+w, x, y, z = 100, -300, 400, -300
+text = 'is minimum value'
+text_2 = 'are minimal and equal'
+min_num = min(w, x, y, z)
+
+# if_elif_else statement without min variables
+if w <= x and w <= y and w <= z:
+    _log.info("'w' is the minimum value")
+elif x <= w and x <= y and x <= z:
+    _log.info("'x' is the minimum value")
+elif y <= w and y <= x and y <= z:
+    _log.info("'y' is the minimum value")
+else:
+    _log.info("'z' is the minimum value")
+
+# fixed if_elif_else statement
+# but in this statement we can`t know which variables are equal
+if min_num == w and min_num == x and min_num == y and min_num == z:
+    _log.info(f'All values {text_2}')
+elif min_num == w and min_num <= x and min_num <= y and min_num <= z:
+    _log.info(f'"w" {text}')
+elif min_num == x and min_num <= w and min_num <= z and min_num <= y:
+    _log.info(f'"x" {text}')
+elif min_num == y and min_num <= w and min_num <= z and min_num <= x:
+    _log.info(f'"y" {text}')
+elif min_num == z and min_num <= w and min_num <= x and min_num <= y:
+    _log.info(f'"z" {text}')
+else:
+    _log.info('We have a problem')
+
+
+
+# if_elif_else statement
+if min_num == w and min_num == x and min_num == y and min_num == z:
+    _log.info(f'All values {text_2}')
+elif min_num == w or min_num == x or min_num == y or min_num == z:
+    if min_num == w and min_num == z and min_num == y and min_num != x:
+        _log.info(f'"w" , "y" and "z" {text_2}')
+    elif min_num == w and min_num == x and min_num == y and  min_num != z:
+        _log.info(f'"w" , "x" and "y" {text_2}')
+    elif min_num == x and min_num == y and min_num == z and min_num != w:
+        _log.info(f'"x" , "y" and "z" {text_2}')
+    elif min_num == w and min_num == x and min_num == z and min_num != y:
+        _log.info(f'"w" , "x" and "z" {text_2}')
+    elif min_num == w and min_num == x and min_num != y and min_num != z:
+        _log.info(f'"w" and "x" {text_2}')
+    elif min_num == w and min_num == y and min_num != x and min_num != z:
+        _log.info(f'"w" and "y" {text_2}')
+    elif min_num == w and min_num == z and min_num != y and min_num != x:
+        _log.info(f'"w" and "z" {text_2}')
+    elif min_num == x and min_num == y and min_num != w and min_num != z:
+        _log.info(f'"x" and "y" {text_2}')
+    elif min_num == x and min_num == z and min_num != y and min_num != w:
+        _log.info(f'"x" and "z" {text_2}')
+    elif min_num == z and min_num == y and min_num != x and min_num != w:
+        _log.info(f'"z" and "y" {text_2}')
+    elif min_num != w and min_num != z and min_num != x:
+        _log.info(f'"y" {text}')
+    elif min_num != w and min_num != z and min_num != y:
+        _log.info(f'"x" {text}')
+    elif min_num != x and min_num != y and min_num != z:
+        _log.info(f'"w" {text}')
+    else:
+        _log.info(f'"z" {text}')
+else:
+    _log.info('We have a problem')
